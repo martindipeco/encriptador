91475b605eeece1c38ed4e5b663bb064b96115b7
martindipeco <martindipeco@gmail.com>
2024-02-09 12:15:01 -0300

    limpieza general

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 //para el menu hamburguesa
-function myMenu() { //previously myFunction
+//función myMenu se activa cuando se hace click en el icono de las barras
+function myMenu() { 
+    //el id my nav bar corresponde al bloque de la barra superior
     var x = document.getElementById("my-nav-bar");
+    //siempre por defecto estará cargada la clase nav bar res
     if (x.className === "nav-bar-resp") {
-      x.className += " responsive";
+        //le cambio el nombre a la clase para que tome otros css
+        x.className += " responsive";
     } else {
-      x.className = "nav-bar-resp";
+        //caso contrario, si ya era responsive, vuelve a normal
+        x.className = "nav-bar-resp";
     }
 }
 
 //resto de funciones
-
-let numeroMaximo = 26;
+//asumo alfabeto norma ASCII de 26 caracteres
+//deberìa cambiarlo si pienso incluir caracteres como la Ñ o la Ç
+const numeroMaximo = 26;
 
 const rangoCifrado = 25;
 
 let rangoCifradoInput = document.getElementById("cifrado");
-
+//establezco atributos de valor maximo y minimo al numero de cifrado
 rangoCifradoInput.setAttribute("max", rangoCifrado);
 rangoCifradoInput.setAttribute("min", -rangoCifrado);
 
-function convertirARango(numero) {
-  // Usa el operador de módulo para obtener el resto de la división entre el número y 51 (25 * 2 + 1)
-  // Luego, resta 25 para asegurarte de que el resultado esté en el rango de -25 a 25
-  return ((numero % 51) + 51) % 51 - 25;
-}
-
-
+//uso esta función para que aparezca el texto cifrado en la segunda caja
 function asignarTextoElemento(elementoID, texto) {
     let elementoHTML = document.getElementById(elementoID);
     //debo usar "value" x q lo estoy asignando a un input. si fuera un contenido normal, sería "elementoHTML.innerHTML = texto;"
@@ -33,6 +33,7 @@ function asignarTextoElemento(elementoID, texto) {
     return;
 }
 
+//al apretar botón enviar
 function enviarMensaje()
 {
     //capturar texto
@@ -60,26 +61,15 @@ function enviarMensaje()
         alert("Este número no generará ningún cambio. Elija un número entre 1 y 25");
         return;
     }
-    /*
-    if (numeroCifrado < 1 || numeroCifrado > 25)
-    {
-        alert("El número debe estar entre 1 y 25");
-        borrar();
-        //TODO: borrar solo el numero, no todo el mensaje
-        //TODO: trabajar con % para asegurarse numero correcto sin tener que restringir
-        //TODO: ver problema con negativos para des encriptar
-        return;
-    }
-    */
-    //numeroCifrado = convertirARango(numeroCifrado);
-    //aplicar cifra
+
+    //comienzo iteración por la frase, aplicando cifra en cada caracter si corresponde 
     let textoCifrado = "";
     let posicionAscii = 0;
     for (i = 0; i < texto.length; i++)
     {
-        //TODO: refactorizar, crear una función a la que le paso valores ascii max y min
-        //TODO: manejar el caso de las vocales acentuadas, ñ y cedilla, por ejemplo
-        //A es 65 y Z es 90
+        //mayúscula A es 65 y Z es 90
+        //si el caracter está fuera de rango, significa que es un caracter especial, por lo tanto se deja sin encriptar
+        //los signos especiales como ? ! , : y etc no se encriptan
         if (texto.charCodeAt(i) >= 65 && texto.charCodeAt(i)<= 90)
         {
             posicionAscii = texto.charCodeAt(i) + numeroCifrado;
@@ -96,7 +86,7 @@ function enviarMensaje()
             //asigno nuevo caracter de acuerdo a codigo ascii, y lo voy sumando a la cadena
             textoCifrado += String.fromCharCode(posicionAscii);
         }
-        //a es 97 y z es 122
+        //minúscula a es 97 y z es 122
         else if (texto.charCodeAt(i) >= 97 && texto.charCodeAt(i) <= 122)
         {
             posicionAscii = texto.charCodeAt(i) + numeroCifrado;
